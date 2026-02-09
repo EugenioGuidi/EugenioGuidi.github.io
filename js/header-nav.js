d5b3ad71e1a2ee91c184dc3f1930283ab03c10b6
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2026-02-09 17:29:03 +0100

    add logo img

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://eugenioguidi.github.io/html/header-nav.html").then(r => r.text()).then(html => {
+    fetch("https://eugenioguidi.github.io/html/template/header-nav.html").then(r => r.text()).then(html => {
         document.getElementById("header-nav").innerHTML = html;
     }).catch(err => console.error("Errore header:", err));
 });
