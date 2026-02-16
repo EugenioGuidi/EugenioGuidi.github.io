0e161978fa8b55166bdb3a31a80c2c9b8659b226
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2026-02-16 16:29:20 +0100

    add script for burger nav

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://eugenioguidi.github.io/html/template/header-nav.html").then(r => r.text()).then(html => {
-        document.getElementById("header-nav").innerHTML = html;
-    }).catch(err => console.error("Errore header:", err));
+    fetch("https://eugenioguidi.github.io/html/template/header-nav.html")
+        .then(r => r.text())
+        .then(html => { 
+            document.getElementById("header-nav").innerHTML = html;
+
+            document.querySelectorAll(".submenu-toggle").forEach(button => {
+                button.addEventListener("click", function() {
+                    this.nextElementSibling.classList.toggle("active");
+                });
+            });
+        })
+        .catch(err => console.error("Errore header:", err));
 });
+
+function toggleMenu() {
+    document.getElementById("mobileMenu").classList.toggle("active");
+}
